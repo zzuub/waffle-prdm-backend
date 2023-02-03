550a72c3bc260eb1d9db6581fbfba684c6dad437
Reese <ajw5921@gmail.com>
2023-02-03 22:54:04 +0900

    Project Setting

---
+++ src/test/java/com/pr_dm/eco/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pr_dm.eco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
