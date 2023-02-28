657d99f78e2e1c3ae33f3f66da879766d643b687
windowdong11 <windowdong11@naver.com>
2023-02-28 23:38:07 +0900

    chore: /post endpoint api versioning 추가

--- src/main/java/com/pr_dm/eco/post/controller/PostController.java
+++ src/main/java/com/pr_dm/eco/post/controller/PostController.java
@@ -8,40 +8,39 @@ import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/post")
 @RequiredArgsConstructor
 public class PostController {
 
     private final PostService postService;
 
     // TODO : authorization header -> get user id
-    @PutMapping("/")
+    @PutMapping("/api/v1/post")
     @ResponseBody
     public PostResponseDto createPost(@RequestBody PostRequestDto req, @RequestHeader String authorization) {
         return this.postService.createPost(req);
     }
 
-    @GetMapping("/list")
+    @GetMapping("/api/v1/post/list")
     @ResponseBody
     public Page<PostResponseDto> getPostLst(@RequestParam(value = "page", defaultValue = "0") int page, @RequestParam(value = "size", defaultValue = "10") int size) {
         return this.postService.getPostList(page, size);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/api/v1/post/{id}")
     @ResponseBody
     public PostResponseDto getPost(@PathVariable("id") long id) {
         return this.postService.getPostById(id);
     }
 
     // TODO : validate authorization header
-    @PostMapping("/{id}")
+    @PostMapping("/api/v1/post/{id}")
     @ResponseBody
     public PostResponseDto updatePost(@PathVariable("id") long id, @RequestBody PostRequestDto req, @RequestHeader String authorization) {
         return this.postService.updatePost(id, req);
     }
 
     // TODO : validate authorization header
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/api/v1/post/{id}")
     @ResponseBody
     public PostResponseDto deletePost(@PathVariable("id") long id, @RequestHeader String authorization) {
         return this.postService.deletePost(id);
